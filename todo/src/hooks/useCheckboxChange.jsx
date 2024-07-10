60359115f3376f2dcb495fc339eea7da8f41ff09
parkseyoung <hellosy76@naver.com>
2024-07-10 15:21:45 +0900

    0710 todo 설명주석

--- todo/src/hooks/useCheckboxChange.jsx
+++ todo/src/hooks/useCheckboxChange.jsx
@@ -1,9 +1,9 @@
 // src/hooks/useCheckboxChange.js
 const useCheckboxChange = ({ todos, setTodos }) => {
     const handleCheckboxChange = (index) => {
-      const newTodos = [...todos];//새로운 배열의 생성
+      const newTodos = [...todos];//새로운 배열의 생성 -> 원본배열을 직접 수정하지 않고 불변성 유지 가능
       newTodos[index].completed = !newTodos[index].completed;
-      setTodos(newTodos);
+      setTodos(newTodos); //todos 상태를 업데이트 하는 함수
     };
   
     return handleCheckboxChange;
